a2d841baae0551d6bc7e1a84c92340fab977ff7b
Inner-Daemons <magnus.larsson.mn@gmail.com>
2026-02-14 22:36:08 -0600

    Removed std feature

--- parse/src/lib.rs
+++ parse/src/lib.rs
@@ -17,7 +17,7 @@
 //!
 //! The error is reported via an opaque `ParseHexfError` type.
 
-#![cfg_attr(not(feature = "std"), no_std)]
+#![no_std]
 use core::{f32, f64, fmt, isize, str};
 
 /// An opaque error type from `parse_hexf32` and `parse_hexf64`.
@@ -59,14 +59,6 @@ impl fmt::Display for ParseHexfError {
     }
 }
 
-#[cfg(feature = "std")]
-impl std::error::Error for ParseHexfError {
-    fn description(&self) -> &'static str {
-        self.text()
-    }
-}
-
-#[cfg(not(feature = "std"))]
 impl core::error::Error for ParseHexfError {
     fn description(&self) -> &'static str {
         self.text()
